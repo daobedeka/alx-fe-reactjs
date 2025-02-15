ca7f88e824d73454f9c22bb9eff72dce785f07c0
Daniel Obedeka <daobedeka@gmail.com>
2025-02-15 16:36:40 +0000

    completed task3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'
 
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 
@@ -40,6 +41,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
