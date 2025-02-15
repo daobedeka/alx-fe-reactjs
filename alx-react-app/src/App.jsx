87b3fbab453b52f3033efb439b3dcd1ee0c5f251
Daniel Obedeka <daobedeka@gmail.com>
2025-02-15 15:47:14 +0000

    completed task2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 
 function App() {
@@ -32,6 +36,10 @@ function App() {
       </p>
 
       <WelcomeMessage />
+
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
