ce79403d6bfc774567d8631ba142ee06e140d157
Daniel Obedeka <daobedeka@gmail.com>
2025-02-23 23:17:36 +0000

    added css style

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 function Footer(){
     return(
-    <footer>
-        <p>© 2023 City Lovers</p>
+    <footer style={{
+      width:"100%vw",
+      border:'1px solid black',
+      
+    }}>
+        <p style={{
+          fontFamily:'cursive',
+          textAlign:'center'
+        }}>© 2023 City Lovers</p>
       </footer>
       )
 }
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,17 @@
 function MainContent(){
     return(
-        <main>
-                <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={
+            {
+                border:'1px solid gray',
+                padding:'10px',
+                margin:'10px'
+            }
+        }>
+                <p style={{
+                    fontFamily:'cursive',
+                    fontSize:'25px',
+                    textAlign:"center",
+                }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
 }
