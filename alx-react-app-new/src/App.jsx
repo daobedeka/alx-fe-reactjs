02230b71a67c2b5318bf59eb5fdda166f23d1e41
Daniel Obedeka <daobedeka@gmail.com>
2025-02-24 20:19:41 +0000

    comm

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -10,7 +10,7 @@ import UserProfile from './components/UserProfile'
 
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 
-import Counter from './components/Counter.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +22,7 @@ function App() {
       <Header />
       <MainContent />
 
-      <Counter />
+  
 
       <Footer />
 
