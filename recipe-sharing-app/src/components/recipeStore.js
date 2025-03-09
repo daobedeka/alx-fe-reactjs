9297fe6d5f3dd966ac387c9933d4e22c35aad45b
Daniel Obedeka <daobedeka@gmail.com>
2025-03-09 23:23:54 +0000

    move

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import {create}  from 'zustand'
 export const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  setRecipies:(updateRecipe)=>set({recipies}),
+  setRecipies:(deleteRecipe)=>set([])
 }));
 
